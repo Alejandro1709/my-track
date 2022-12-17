79a9ee029710d904c22b9b04676c24cba0248f12
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-12-17 16:10:46 -0500

    Add: Sidebar & Main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div className={styles.AppContainer}>
+      <aside className={styles.AppSidebar}>Sidebar</aside>
+      <main className={styles.AppMain}>
+        <header className={styles.AppHeader}>Ingeniería de software</header>
+      </main>
     </div>
   );
 }
