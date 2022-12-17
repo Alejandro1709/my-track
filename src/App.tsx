3e10c77225087866852e322aaa89e50340e5bf63
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-12-17 16:16:40 -0500

    Add: moved AppContainer to a component file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import AppContainer from './components/AppContainer';
 import styles from './App.module.css';
 
 function App() {
   return (
-    <div className={styles.AppContainer}>
+    <AppContainer>
       <aside className={styles.AppSidebar}>Sidebar</aside>
       <main className={styles.AppMain}>
         <header className={styles.AppHeader}>Ingeniería de software</header>
       </main>
-    </div>
+    </AppContainer>
   );
 }
 
