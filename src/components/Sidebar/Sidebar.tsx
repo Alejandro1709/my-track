013e5713caca157863b48f0d1b35f4a96ff3d273
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-12-19 19:05:25 -0500

    Add: React icons and add sidebar icons

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,22 @@
+import { AiFillFilter, AiOutlineGroup } from 'react-icons/ai';
 import styles from './Sidebar.module.css';
 
 function Sidebar() {
-  return <aside className={styles.Sidebar}>Sidebar</aside>;
+  return (
+    <aside className={styles.Sidebar}>
+      <div className={styles.SidebarInner}>
+        <div className={styles.SidebarLogo}>LOGO</div>
+        <div className={styles.SidebarActions}>
+          <div className={styles.Filter}>
+            <AiFillFilter />
+          </div>
+          <div className={styles.Order}>
+            <AiOutlineGroup />
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
 }
 
 export default Sidebar;
